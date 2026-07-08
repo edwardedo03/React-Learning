efe553d85d1d685bcedacb20d89e7621a9861201
edwardedo03 <edwardahhutauruk@gmail.com>
2026-07-09 00:16:55 +0700

    fix event_planner

---
+++ event_planner/src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer>© Event Planner Organization. All rights reserved.</footer>
+    </div>
+  );
+};
+
+export default Footer;
